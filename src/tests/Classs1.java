a36a876fedb1c582ed57e0784fb4b91285e29a81
Ayse3434 <aysegul2013x@gmail.com>
2023-06-16 14:21:23 +0200

    first mesaj

--- src/tests/Classs1.java
+++ src/tests/Classs1.java
@@ -5,9 +5,10 @@ public class Classs1 {
     //Merhaba
 
     public static void main(String[] args) {
-      
+
         System.out.println("merhaba");
         System.out.println();
+        System.out.println("Aysegul ");
       
     }
 
